7a429ae972588e1733a9c20cde29f5a9c792a93e
Jean <jeanmarcmaree@protonmail.com>
2022-06-16 12:50:31 +0200

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'unused-imports'],
+    extends: ['airbnb'],
+    rules: {
+        'eslint:recommended': [0],
+        'plugin:@typescript-eslint/recommended': [0],
+        'newline-before-return': [0],
+        'newline-after-var': [0],
+        'no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'error',
+            {
+                vars: 'all',
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                argsIgnorePattern: '^_'
+            }
+        ],
+        'sort-imports': [
+            'error',
+            {
+                ignoreDeclarationSort: true
+            }
+        ],
+
+        semi: [0],
+        '@typescript-eslint/indent': ['error', 4],
+        eqeqeq: [0],
+        'prefer-const': [0],
+        'no-const-assign': [0],
+        'no-var': 'error',
+        'comma-dangle': [
+            'error',
+            {
+                arrays: 'never',
+                objects: 'never',
+                imports: 'never',
+                exports: 'never',
+                functions: 'never'
+            }
+        ],
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+        'arrow-parens': [0],
+        'no-console': 'off',
+        indent: ['error', 4],
+        'no-underscore-dangle': 'off'
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+console.log('HEllo World');
+let a = 0;
+const _b = 2;
+
+if (a === 1) {
+    console.log('3');
+} else {
+    console.log('2');
+}
